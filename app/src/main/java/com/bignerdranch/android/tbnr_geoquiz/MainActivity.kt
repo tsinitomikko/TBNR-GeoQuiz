19570e2151521cabb404183a0970cfc47033cc7e
Mikko <18387920+tsinitomikko@users.noreply.github.com>
2023-08-01 13:24:54 -0500

    feat: chapter 1 - your first android application

--- app/src/main/java/com/bignerdranch/android/tbnr_geoquiz/MainActivity.kt
+++ app/src/main/java/com/bignerdranch/android/tbnr_geoquiz/MainActivity.kt
@@ -1,11 +1,29 @@
 package com.bignerdranch.android.tbnr_geoquiz
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var trueButton: Button
+    private lateinit var falseButton: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        setTitle(R.string.app_name)
+
+        trueButton = findViewById(R.id.true_button)
+        falseButton = findViewById(R.id.false_button)
+
+        trueButton.setOnClickListener {
+            Toast.makeText(this, R.string.correct_toast, Toast.LENGTH_SHORT).show()
+        }
+        falseButton.setOnClickListener {
+            Toast.makeText(this, R.string.incorrect_toast, Toast.LENGTH_SHORT).show()
+        }
     }
 }
