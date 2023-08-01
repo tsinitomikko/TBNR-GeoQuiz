ad318946fa208d0f8758d9e34731edb6eb54bc38
Mikko <18387920+tsinitomikko@users.noreply.github.com>
2023-08-01 16:52:23 -0500

    feat: chapter 1 - switching your toast for a snackbar (challenge)

--- app/src/main/java/com/bignerdranch/android/tbnr_geoquiz/MainActivity.kt
+++ app/src/main/java/com/bignerdranch/android/tbnr_geoquiz/MainActivity.kt
@@ -2,8 +2,8 @@ package com.bignerdranch.android.tbnr_geoquiz
 
 import android.os.Bundle
 import android.widget.Button
-import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.snackbar.Snackbar
 
 class MainActivity : AppCompatActivity() {
 
@@ -20,10 +20,10 @@ class MainActivity : AppCompatActivity() {
         falseButton = findViewById(R.id.false_button)
 
         trueButton.setOnClickListener {
-            Toast.makeText(this, R.string.correct_toast, Toast.LENGTH_SHORT).show()
+            Snackbar.make(it, R.string.correct_toast, Snackbar.LENGTH_SHORT).show()
         }
         falseButton.setOnClickListener {
-            Toast.makeText(this, R.string.incorrect_toast, Toast.LENGTH_SHORT).show()
+            Snackbar.make(it, R.string.incorrect_toast, Snackbar.LENGTH_SHORT).show()
         }
     }
 }
