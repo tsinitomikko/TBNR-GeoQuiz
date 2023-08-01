95d490984ea20d8a2786d3c654a49375ad882bab
Mikko <18387920+tsinitomikko@users.noreply.github.com>
2023-08-01 17:25:05 -0500

    feat: chapter 2 - interactive user interfaces

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,12 +5,12 @@ plugins {
 
 android {
     namespace = "com.bignerdranch.android.tbnr_geoquiz"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.bignerdranch.android.tbnr_geoquiz"
         minSdk = 24
-        targetSdk = 33
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
 
@@ -32,6 +32,9 @@ android {
     }
     kotlinOptions {
         jvmTarget = "1.8"
+    }
+    buildFeatures {
+        viewBinding = true
     }
 }
 
---
+++ app/src/main/java/com/bignerdranch/android/tbnr_geoquiz/Question.kt
@@ -0,0 +1,5 @@
+package com.bignerdranch.android.tbnr_geoquiz
+
+import androidx.annotation.StringRes
+
+data class Question(@StringRes val textResId: Int, val answer: Boolean)
