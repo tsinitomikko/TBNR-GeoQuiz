ecd616eb4e83e44416eef834e1dd62405ef0489c
Mikko <18387920+tsinitomikko@users.noreply.github.com>
2023-08-02 02:24:30 -0500

    feat: chapter 6 - testing

---
+++ app/src/test/java/com/bignerdranch/android/tbnr_geoquiz/QuizViewModelTest.kt
@@ -0,0 +1,24 @@
+package com.bignerdranch.android.tbnr_geoquiz
+
+import androidx.lifecycle.SavedStateHandle
+import org.junit.Assert
+import org.junit.Test
+
+class QuizViewModelTest {
+
+    @Test
+    fun providesExpectedQuestionText() {
+        val savedStateHandle = SavedStateHandle()
+        val quizViewModel = QuizViewModel(savedStateHandle)
+        Assert.assertEquals(R.string.question_australia, quizViewModel.currentQuestionText)
+    }
+
+    @Test
+    fun wrapsAroundQuestionBank() {
+        val savedStateHandle = SavedStateHandle(mapOf(CURRENT_INDEX_KEY to 5))
+        val quizViewModel = QuizViewModel(savedStateHandle)
+        Assert.assertEquals(R.string.question_asia, quizViewModel.currentQuestionText)
+        quizViewModel.moveToNext()
+        Assert.assertEquals(R.string.question_australia, quizViewModel.currentQuestionText)
+    }
+}
