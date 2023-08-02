ffb49d810c5cd4fe72f24f47bf43cb5c7ad6d5cd
Mikko <18387920+tsinitomikko@users.noreply.github.com>
2023-08-02 02:24:30 -0500

    feat: chapter 6 - testing

---
+++ app/src/androidTest/java/com/bignerdranch/android/tbnr_geoquiz/MainActivityTest.kt
@@ -0,0 +1,50 @@
+package com.bignerdranch.android.tbnr_geoquiz
+
+import androidx.test.core.app.ActivityScenario
+import androidx.test.core.app.ActivityScenario.launch
+import androidx.test.espresso.Espresso
+import androidx.test.espresso.action.ViewActions
+import androidx.test.espresso.assertion.ViewAssertions
+import androidx.test.espresso.matcher.ViewMatchers
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import org.junit.After
+import org.junit.Before
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@RunWith(AndroidJUnit4::class)
+class MainActivityTest {
+
+    private lateinit var scenario: ActivityScenario<MainActivity>
+
+    @Before
+    fun setUp() {
+        scenario = launch(MainActivity::class.java)
+    }
+
+    @After
+    fun tearDown() {
+        scenario.close()
+    }
+
+    @Test
+    fun showsFirstQuestionOnLaunch() {
+        Espresso.onView(ViewMatchers.withId(R.id.question_text_view))
+            .check(ViewAssertions.matches(ViewMatchers.withText(R.string.question_australia)))
+    }
+
+    @Test
+    fun showsSecondQuestionAfterNextPress() {
+        Espresso.onView(ViewMatchers.withId(R.id.next_button)).perform(ViewActions.click())
+        Espresso.onView(ViewMatchers.withId(R.id.question_text_view))
+            .check(ViewAssertions.matches(ViewMatchers.withText(R.string.question_oceans)))
+    }
+
+    @Test
+    fun handlesActivityRecreation() {
+        Espresso.onView(ViewMatchers.withId(R.id.next_button)).perform(ViewActions.click())
+        scenario.recreate()
+        Espresso.onView(ViewMatchers.withId(R.id.question_text_view))
+            .check(ViewAssertions.matches(ViewMatchers.withText(R.string.question_oceans)))
+    }
+}
